f9c8c7f7c31c615958db10aa5291d219a056d7c0
邓鄧等灯 <178024710@qq.com>
2018-08-18 11:57:49 +0800

    Update App.java
    适配7.0以上，动态获取监听

--- app/src/main/java/com/deng/netmonitor/App.java
+++ app/src/main/java/com/deng/netmonitor/App.java
@@ -12,6 +12,15 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //动态注册网络变化广播
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //实例化IntentFilter对象
+            IntentFilter filter = new IntentFilter();
+            filter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+            NetConnectionReceiver netBroadcastReceiver = new NetConnectionReceiver();
+            //注册广播接收
+            registerReceiver(netBroadcastReceiver, filter);
+        }
         /*开启网络广播监听*/
         NetStateReceiver.registerNetworkStateReceiver(this);
     }
